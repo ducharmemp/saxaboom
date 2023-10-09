b03256b2ab8fe72aebd5a9ce228b1b800b096efb
Matthew DuCharme <ducharmemp@gmail.com>
2023-10-08 23:18:25 -0400

    Fixing up bin script

--- bin/release.sh
+++ bin/release.sh
@@ -6,13 +6,15 @@ set -eou pipefail
 mix format --check-formatted
 mix credo
 mix test
-grep -q "$VERSION" CHANGELOG.md || (echo "Make sure that the release notes are up to date" && false)
 
 VERSION=$1
+
+grep -q "$VERSION" CHANGELOG.md || (echo "Make sure that the release notes are up to date" && false)
 sed -i "s/@version .*/@version \"$VERSION\"/g" mix.exs
 sed -i "s/{:saxaboom, .*/{:saxaboom, \"$VERSION\"}/g" README.md
-git add mix.exs
+git add mix.exs CHANGELOG.md README.md
 git commit -m "Releasing ${VERSION}"
 git tag v$VERSION
+git push
 git push --tags
 mix hex.publish
