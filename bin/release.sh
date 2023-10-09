808b2dfcdebd7af246e2fbbab53e537aa4a9a1f0
Matthew DuCharme <ducharmemp@gmail.com>
2023-10-08 23:17:01 -0400

    Releasing 0.1.2

--- bin/release.sh
+++ bin/release.sh
@@ -10,6 +10,7 @@ grep -q "$VERSION" CHANGELOG.md || (echo "Make sure that the release notes are u
 
 VERSION=$1
 sed -i "s/@version .*/@version \"$VERSION\"/g" mix.exs
+sed -i "s/{:saxaboom, .*/{:saxaboom, \"$VERSION\"}/g" README.md
 git add mix.exs
 git commit -m "Releasing ${VERSION}"
 git tag v$VERSION
